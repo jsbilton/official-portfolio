96b9b0e46c2a135cc346ce5836be9e7b97c6d569
Jared Bilton <jsbilton@gmail.com>
2017-02-07 15:21:22 -0500

    setup-about-home

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+const React = require('react')
+
+const About = React.createClass({
+  render () {
+    return (
+      <div>
+        <h3>Allow myself to introduce myself</h3>
+      </div>
+    )
+  }
+})
+module.exports = About
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Welcome Home</h2>
+      </div>
+    )
+  }
+})
+
+module.exports = Home
